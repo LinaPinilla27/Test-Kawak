f211b32d0e2d4003c53fa061400feabd1310a4b1
Lina <lina@MacBook-Pro-de-Lina.local>
2023-12-23 15:39:37 -0500

    fix list

--- src/components/ListaNotas.vue
+++ src/components/ListaNotas.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <h2 class="my-4">Lista de Eventos</h2>
+    
+    <!-- Agregar campo de búsqueda -->
+    <div class="mb-3">
+      <label for="searchInput" class="form-label">Buscar por nombre:</label>
+      <input type="text" v-model="searchTerm" class="form-control" id="searchInput">
+    </div>
+
     <table class="table">
       <thead class="thead-dark">
         <tr>
@@ -12,7 +19,8 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="event in events" :key="event.id">
+        <!-- Filtrar eventos por nombre -->
+        <tr v-for="event in filteredEvents" :key="event.id">
           <td>{{ event.id }}</td>
           <td>{{ event.title }}</td>
           <td>{{ event.start }}</td>
@@ -31,8 +39,19 @@ import { defineComponent } from 'vue';
 import { mapGetters, mapActions } from 'vuex';
 
 export default defineComponent({
+  data() {
+    return {
+      searchTerm: '', // Nuevo dato para almacenar el término de búsqueda
+    };
+  },
   computed: {
     ...mapGetters(['events']),
+    // Filtrar eventos por nombre
+    filteredEvents() {
+      return this.events.filter(event =>
+        event.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    },
   },
   methods: {
     ...mapActions(['removeEvent']),
